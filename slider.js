f135ea1230a640cf51db4e03d580b6ad05d2346e
letsdothispej <pauljamesazanes@gmail.com>
2021-06-05 02:08:14 +0800

    mobile style

--- slider.js
+++ slider.js
@@ -9,16 +9,12 @@ const testimonialStatementContainer = document.querySelector(".testimonial--stat
  testimonialStudentJobRole = testimonialStatementContainer.querySelector(".testimonial__student__job-role");
 
 const testimonials = [{
- statement: `“ I’ve been interested in coding for a while but never taken the jump, until now. 
- I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
- excited about the future. ”`,
+ statement: `“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”`,
  name: "Tanya Sinclair",
  role: "UX Engineer",
  image: "images/image-tanya.jpg"
 },{
- statement: `“ If you want to lay the best foundation possible I’d recommend taking this course. 
- The depth the instructors go into is incredible. I now feel so confident about 
- starting up as a professional developer. ”`,
+ statement: `“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”`,
  name: "John Tarkpor",
  role: "Junior Front-end Developer",
  image: "images/image-john.jpg"
@@ -44,7 +40,6 @@ function paintTestimonial(n) {
  }
  
  loadTestimonial(testimonials[n]);
- console.log(testimonialIndex);
 }
 
 function handleClickNext(){
